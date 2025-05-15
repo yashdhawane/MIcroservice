b7116cd1ea2e6a7e66562df135d5a8cc8d398c26
Yash Dhawane <Yash.Dhawane@ril.com>
2025-05-15 17:43:31 +0530

    media:utils/model/middleware

---
+++ media-service/utils/logger.js
@@ -0,0 +1,32 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: process.env.NODE_ENV === "production" ? 'info' : 'debug',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.splat(),
+        winston.format.errors({stack: true}),
+        winston.format.json()
+    ),
+    defaultMeta:{ service: 'media-service' },
+    transports:[
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename: 'error.log',
+            level: 'error',
+            dirname: 'logs',
+            handleExceptions: true,
+            maxsize: 5242880, // 5MB
+            maxFiles: 5,
+        }),
+        new winston.transports.File({filename: 'combined.log'}),
+    ],
+});
+
+module.exports = logger;
+
